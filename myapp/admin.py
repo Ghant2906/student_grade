75b9552813e031c1547e7e4d17dbc100bee82847
ghant2906 <thaithang115@gmail.com>
2024-06-11 09:59:17 +0700

    register with admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Classes, Courses, Enrollments, ForumPosts, Grades, Roles, Users 
 
-# Register your models here.
+admin.site.register(Classes)
+admin.site.register(Courses)
+admin.site.register(Enrollments)
+admin.site.register(ForumPosts)
+admin.site.register(Grades)
+admin.site.register(Roles)
+admin.site.register(Users)
