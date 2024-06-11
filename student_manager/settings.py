315dd3d9737eaf7e40bca0b2a4e4e3ed9825050c
ghant2906 <thaithang115@gmail.com>
2024-06-11 15:22:11 +0700

    add disable CSRF

--- student_manager/settings.py
+++ student_manager/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'myapp.middleware.middleware.DisableCSRFCheckMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
